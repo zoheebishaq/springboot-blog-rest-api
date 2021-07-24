9db375f6dec1ac66d3fa8746df3923b27b1c61e7
zoheebishaq <zoheeb.ishaque@gmail.com>
2021-07-24 21:51:30 +0200

    Initial commit

---
+++ src/main/java/com/springboot/blog/service/PostService.java
@@ -0,0 +1,18 @@
+package com.springboot.blog.service;
+
+import com.springboot.blog.dto.PostDto;
+
+import java.util.List;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts(int pageNo, int pageSize);
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+
+}
